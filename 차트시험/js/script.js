5b27c1e847f9c0184f2b170b9d9a687cc66a5596
kimjuneseo <june3929213512@gmail.com>
2022-05-15 18:25:23 +0900

    2번째

--- 차트시험/js/script.js
+++ 차트시험/js/script.js
@@ -30,32 +30,64 @@ const dummy = [
 function render(){
     const limit = 100;
     const length = dummy.length;
-    const [pl,pr,pt,pb] = [50,0,50,10];
+    const [pl,pr,pt,pb] = [50,0,20,30];
+    const maxHeight = (canvasHeight-pt-pb)
     let maxValue = Math.max(...dummy.map(({oldValue, newValue}) => Math.max(oldValue,newValue)));
     maxValue = Math.ceil(maxValue/limit)*limit;
+
     const rowCount = maxValue/limit;
-    const rowLimit = (canvasHeight-pt-pb)/rowCount
+    const rowLimit = maxHeight/rowCount;
     for(let i =0; i <= rowCount; i++){
+        const y = maxHeight+pt-i*rowLimit; 
         ctx.beginPath();
         ctx.fillStyle = '#eee';
-        ctx.fillRect(pl,rowLimit*i-pb,canvasWidth-pr-pl,1);
+        ctx.textAlign = 'right';
+        ctx.fillRect(pl,y,canvasWidth-pr-pl,1);
 
         ctx.textAlign = 'center';
         ctx.fillStyle = '#777';
-        ctx.fillText(limit*i,pl-10,canvasHeight-pt-pb-rowLimit*i-6)
+        ctx.fillText(i*limit,pl-10,y+5);
     }
     const width = 150;
     const px = 50;
-    const gap = canvasWidth-(width*length-1);
-    // const p = 
+    const rowWidth = canvasWidth-pt-pr-px*2;
+    const gap = (rowWidth-width*length)/(length-1);
+    const p = maxHeight/maxValue
     console.log(gap);
-    dummy.map(({oldValue, newValue}, idx) => {
-        const x = idx ? (pl+gap+width)*idx : px+pl
-        console.log(x );
+    dummy.map(({title,oldValue, newValue}, idx) => {
+        const height = p*newValue;
+        const x = pl+px+ (width*idx) + (gap*idx);
+        const y = maxHeight+pt-height;
+        
         ctx.beginPath();
-        ctx.fillRect(x,100,width,100);
+        ctx.fillStyle = '#777';
+        ctx.fillRect(x,y,width,height);
+
+        ctx.font = '16px serif bold';
+        ctx.fillText(title,x + width/2,maxHeight+pt+pb/2);
+        ctx.fillStyle = 'blue';
+        ctx.fillText(newValue,x + width/2,y-10);
+
+    });
+
+    dummy.forEach(({oldValue},idx) => {
+        const x = px + pl + (width*idx) + (gap*idx) + (width/2);
+        const y = maxHeight+pt - oldValue*p;
+        idx ? ctx.lineTo(x,y) : ctx.moveTo(x,y);
+        ctx.stroke();
     })
-    // const barWidth = 
+
+    dummy.forEach(({oldValue},idx) => {
+        const x = px + pl + (width*idx) + (gap*idx) + (width/2);
+        const y = maxHeight+pt - oldValue*p;
+        ctx.beginPath();
+        
+        ctx.fillStyle = 'red';
+        ctx.arc(x,y,5,0,Math.PI * 2);
+        ctx.fill();
+        ctx.fillStyle = 'blue';
+        ctx.fillText(oldValue, x ,y-10);
+    })
 }
 
 render();
